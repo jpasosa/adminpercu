4ac5e6a4f51efe0b07d14307f0605c2363014983
Juan Pablo <jpasosa@gmail.com>
2018-06-08 18:08:41 -0300

    agrego helper ddd() para debuguear sin que muera(die())

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -16,3 +16,27 @@ if ( ! function_exists('saco_porc'))
         return ( 1 - ($porcentaje * 0.01) );
     }
 }
+
+if ( ! function_exists('ddd'))
+{
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  mixed
+     * @return void
+     */
+    function ddd($v, $asString = false)
+    {
+        if (!$asString) {
+            array_map(function ($x) {
+                (new Illuminate\Support\Debug\Dumper)->dump($x);
+            }, [$v]);
+        } else {
+            $r = array_map(function ($x) {
+                return (new App\Library\Classes\Dumper)->dump($x);
+            }, [$v]);
+            return $r[0];
+        }
+    }
+}
+
